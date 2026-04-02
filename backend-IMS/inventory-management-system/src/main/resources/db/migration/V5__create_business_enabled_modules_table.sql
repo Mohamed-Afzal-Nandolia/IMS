0e1f42d646ea3785b623f0a5b3598418df5e8173
afzal.nandolia <afzal.nandolia@gmail.com>
2026-04-02 18:48:12 +0530

    fixed 5th migration script

--- backend-IMS/inventory-management-system/src/main/resources/db/migration/V5__create_business_enabled_modules_table.sql
+++ backend-IMS/inventory-management-system/src/main/resources/db/migration/V5__create_business_enabled_modules_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE business_enabled_modules (
     business_id VARCHAR(36) NOT NULL,
     module VARCHAR(255) NOT NULL,
+    PRIMARY KEY (business_id, module),
     CONSTRAINT fk_business_enabled_modules_business FOREIGN KEY (business_id) REFERENCES businesses (id) ON DELETE CASCADE
 );
